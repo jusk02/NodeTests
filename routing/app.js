cb8ca668628f40d8dcd146e0b7fcbeec43ee3ff6
Juan David Ramirez <juramirez@hugeinc.com>
2017-08-17 18:20:40 -0500

    pipes,streams,routing

--- routing/app.js
+++ routing/app.js
@@ -8,6 +8,9 @@ var server = http.createServer(function(req,res) {
   if (req.url === '/home' || req.url === '/' ) {
     res.writeHead(200,{'Content-Type': 'text/html'});
     fs.createReadStream(__dirname + '/index.html').pipe(res);
+  }else {
+    res.writeHead(200,{'Content-Type': 'text/html'});
+    fs.createReadStream(__dirname + '/index.html').pipe(res);
   }
 
 
