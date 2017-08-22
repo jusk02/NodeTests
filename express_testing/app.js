7c003ce2d2a296864f2e36f2a61927ac0f620545
Juan David Ramirez <juramirez@hugeinc.com>
2017-08-22 17:10:56 -0500

    progress

---
+++ express_testing/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/',function(req,res) {
+  // text response
+  // res.send('this is the home page');
+
+  // html response
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/contact',function(req,res) {
+  res.sendFile(__dirname + '/contact.html');
+});
+
+// route variables or params
+app.get('/profile/:name', function(req,res) {
+  // text response
+  // res.send('you requested to se profile '+ req.params.id)
+  var dummy_data = { age:29, job: 'ninja',hobbies: ['eating','fishing','cars']};
+  // response with EJS
+  res.render('profile',{ person: req.params.name, data: dummy_data });
+});
+
+app.listen(3000);
--- routing/app.js
+++ routing/app.js
@@ -8,9 +8,13 @@ var server = http.createServer(function(req,res) {
   if (req.url === '/home' || req.url === '/' ) {
     res.writeHead(200,{'Content-Type': 'text/html'});
     fs.createReadStream(__dirname + '/index.html').pipe(res);
+  }else if (req.url === '/api/list') {
+    var ninjas = [{ name:'ryu', age:33 },{ name:'ana', age:22} ];
+    res.writeHead(200,{'Content-Type': 'application/json'});
+    res.end(JSON.stringify(ninjas));
   }else {
-    res.writeHead(200,{'Content-Type': 'text/html'});
-    fs.createReadStream(__dirname + '/index.html').pipe(res);
+    res.writeHead(404,{'Content-Type': 'text/html'});
+    fs.createReadStream(__dirname + '/404.html').pipe(res);
   }
 
 
