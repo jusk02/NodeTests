e9426b181e83b549739ae536da00b4496987c769
Juan David Ramirez <juramirez@hugeinc.com>
2017-08-23 17:10:14 -0500

    express first steps

--- express_testing/app.js
+++ express_testing/app.js
@@ -1,20 +1,44 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 var app = express();
 
+var urlencodedParser = bodyParser.urlencoded({extended : false });
+
 app.set('view engine', 'ejs');
+
+// Express middleware
+app.use('/assets', express.static('assets'));
+
+// custom middleware
+// app.use('/assets', function(req,res,next){
+//   console.log(req.url);
+//   next();
+// });
 
 app.get('/',function(req,res) {
   // text response
   // res.send('this is the home page');
 
   // html response
-  res.sendFile(__dirname + '/index.html');
+  // res.sendFile(__dirname + '/index.html');
+
+  res.render('index');
+});
+
+app.get('/query_strings',function(req,res) {
+  console.log(req.query);
+  res.render('query_strings', {qs: req.query});
 });
 
 app.get('/contact',function(req,res) {
   // res.sendFile(__dirname + '/contact.html');
   res.render('contact');
+});
+
+app.post('/contact', urlencodedParser, function(req,res) {
+  console.log(req.body);
+  res.render('contact-success',{data: req.body});
 });
 
 // route variables or params
